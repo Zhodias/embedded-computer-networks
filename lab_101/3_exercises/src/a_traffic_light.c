e217dd55166dc0d93e8b00f9ffb613b5a16b3c80
Zhodias <32358724+Zhodias@users.noreply.github.com>
2017-10-05 12:15:26 +0100

    solved lab 1 exercise 3b

--- lab_101/3_exercises/src/a_traffic_light.c
+++ lab_101/3_exercises/src/a_traffic_light.c
@@ -5,6 +5,7 @@
  *
  */
 
+
 // include the hal drivers
 #include "stm32f7xx_hal.h"
 
@@ -13,9 +14,14 @@
 #include "clock.h"
 #include "gpio.h"
 
-// map the led to GPIO PA8
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
+// map the led to GPIO PI1 (again, this is the inbuilt led)
+gpio_pin_t ledR = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t ledY = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t ledG = {PB_15, GPIOB, GPIO_PIN_15};
 
+gpio_pin_t ledR2 = {PA_8, GPIOA, GPIO_PIN_8};
+gpio_pin_t ledY2 = {PA_15, GPIOA, GPIO_PIN_15};
+gpio_pin_t ledG2 = {PI_2, GPIOI, GPIO_PIN_2};
 // this is the main method
 int main()
 {
@@ -24,16 +30,57 @@ int main()
   HAL_Init();
   init_sysclk_216MHz();
   
-  // initialise the gpio pin
-  init_gpio(led, OUTPUT);
-  
+  // initialise the gpio pins
+		init_gpio(ledR, OUTPUT);
+    init_gpio(ledY, OUTPUT);
+	  init_gpio(ledG, OUTPUT);
+		init_gpio(ledR2, OUTPUT);
+    init_gpio(ledY2, OUTPUT);
+	  init_gpio(ledG2, OUTPUT);
   // loop forever ...
+	
+	toggle_gpio(ledR2);
+	toggle_gpio(ledR);
+	
   while(1)
   {
-    // toggle the led on the gpio pin
-    toggle_gpio(led);
+    // R2 off G2 On
+			toggle_gpio(ledR2);
+			toggle_gpio(ledG2);
+		
+			HAL_Delay(1500);
+			
+		//Y2 On G2 Off
+			toggle_gpio(ledY2);
+			toggle_gpio(ledG2);
+		
+			HAL_Delay(1500);  
+			
+		//Y2 Off R2 On
+			toggle_gpio(ledY2);
+			toggle_gpio(ledR2);
+		
+			HAL_Delay(1500);
+			
+		//R Off G On
+			toggle_gpio(ledR);
+			toggle_gpio(ledG);
+		
+			HAL_Delay(1500);
+			
+		//Y On G Off
+			toggle_gpio(ledY);
+			toggle_gpio(ledG);
+		
+			HAL_Delay(1500);  
+			
+		//Y Off R On
+			toggle_gpio(ledY);
+			toggle_gpio(ledR);
+		
+			HAL_Delay(1500);
+		
     
-    // wait for 1 second
-    HAL_Delay(1000);
   }
 }
+
