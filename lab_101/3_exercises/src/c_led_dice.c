6cb197e83f96eb01cb2ee584a35af2755900e1df
Zhodias <32358724+Zhodias@users.noreply.github.com>
2017-10-05 12:57:16 +0100

    Dice game

--- lab_101/3_exercises/src/c_led_dice.c
+++ lab_101/3_exercises/src/c_led_dice.c
@@ -12,9 +12,15 @@
 #include "pinmappings.h"
 #include "clock.h"
 #include "gpio.h"
+#include "random_numbers.h"
 
 // map the led to GPIO PA8
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led1 = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led2 = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t led3 = {PB_15, GPIOB, GPIO_PIN_15};
+gpio_pin_t led4 = {PA_8, GPIOA, GPIO_PIN_8};
+gpio_pin_t led5 = {PA_15, GPIOA, GPIO_PIN_15};
+gpio_pin_t led6 = {PI_2, GPIOI, GPIO_PIN_2};
 
 // this is the main method
 int main()
@@ -25,15 +31,68 @@ int main()
   init_sysclk_216MHz();
   
   // initialise the gpio pin
-  init_gpio(led, OUTPUT);
   
   // loop forever ...
   while(1)
   {
-    // toggle the led on the gpio pin
-    toggle_gpio(led);
-    
-    // wait for 1 second
-    HAL_Delay(1000);
+		
+		uint32_t rnd = (get_random_int() % 6) + 1;
+		
+		//Roll 1
+		if(rnd == 1)
+		{
+			toggle_gpio(led1);
+		}
+		//Roll 2
+		else if(rnd == 2)
+		{
+			toggle_gpio(led1);
+			toggle_gpio(led2);
+		}
+		//Roll 3
+		else if(rnd == 3)
+		{
+			toggle_gpio(led1);
+			toggle_gpio(led2);
+			toggle_gpio(led3);
+		}
+		//Roll 4
+		else if(rnd == 4)
+		{
+			toggle_gpio(led1);
+			toggle_gpio(led2);
+			toggle_gpio(led3);
+			toggle_gpio(led4);
+		}
+		//Roll 5
+		else if(rnd == 5)
+		{
+			toggle_gpio(led1);
+			toggle_gpio(led2);
+			toggle_gpio(led3);
+			toggle_gpio(led4);
+			toggle_gpio(led5);
+		}
+		//Roll 6
+		else if(rnd == 6)
+		{
+			toggle_gpio(led1);
+			toggle_gpio(led2);
+			toggle_gpio(led3);
+			toggle_gpio(led4);
+			toggle_gpio(led5);
+			toggle_gpio(led6);
+		}
+
+    // wait for 10 second
+    HAL_Delay(10000);
+		write_gpio(led1, LOW);
+		write_gpio(led2, LOW);
+		write_gpio(led3, LOW);
+		write_gpio(led4, LOW);
+		write_gpio(led5, LOW);
+		write_gpio(led6, LOW);
+		
+		
   }
 }
